e1637bef3de59306d479f1aabff7c006ea0db1f1
Hanna <hanna@Hannas-MacBook-Air.local>
2019-11-16 16:17:52 +0300

    added JavaScript

---
+++ script.js
@@ -0,0 +1,36 @@
+let blackAndWhiteButton = document.querySelector("#black-n-white-btn");
+let coloredButton = document.querySelector("#colored-btn");
+const darkModeButton = document.querySelector("#dark-mode-btn");
+const bodyTag = document.querySelector("body");
+const h1Tag = document.querySelector("h1");
+const hrTags = document.querySelectorAll("hr");
+
+blackAndWhiteButton.addEventListener("click", ()=>{
+	bodyTag.classList.remove("dark-mode", "colored-mode");
+	h1Tag.classList.remove("dark-mode-h1", "colored-mode-h1");
+	for (let i = 0; i < hrTags.length; i++){
+       hrTags[i].classList.remove("dark-mode-hr", "colored-mode-hr");
+	}
+})
+
+coloredButton.addEventListener("click", ()=>{
+	bodyTag.classList.remove("dark-mode");
+	bodyTag.classList.add("colored-mode");
+	h1Tag.classList.remove("dark-mode-h1");
+	h1Tag.classList.add("colored-mode-h1");
+	for (let i = 0; i < hrTags.length; i++){
+		hrTags[1].classList.remove("dark-mode-hr");
+		hrTags[i].classList.add("colored-mode-hr");
+	}
+});
+
+darkModeButton.addEventListener("click", ()=>{
+	bodyTag.classList.remove("colored-mode");
+	bodyTag.classList.add("dark-mode");
+	h1Tag.classList.remove("colored-mode-h1");
+	h1Tag.classList.add("dark-mode-h1");
+	for (let i = 0; i < hrTags.length; i++){
+		hrTags[i].classList.remove("colored-mode-hr");
+        hrTags[i].classList.add("dark-mode-hr");
+	}
+});
