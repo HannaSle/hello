c6aee7699ec23c351d8547fb83171eb7da5e3958
Hanna <hanna@Hannas-MacBook-Air.local>
2019-11-16 16:43:00 +0300

    removed icon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let blackAndWhiteButton = document.querySelector("#black-n-white-btn");
-let coloredButton = document.querySelector("#colored-btn");
-const darkModeButton = document.querySelector("#dark-mode-btn");
+const blackAndWhiteButton = document.querySelector("#black-n-white-btn");
 const bodyTag = document.querySelector("body");
+const coloredButton = document.querySelector("#colored-btn");
+const darkModeButton = document.querySelector("#dark-mode-btn");
 const h1Tag = document.querySelector("h1");
 const hrTags = document.querySelectorAll("hr");
 
@@ -10,8 +10,8 @@ blackAndWhiteButton.addEventListener("click", ()=>{
 	h1Tag.classList.remove("dark-mode-h1", "colored-mode-h1");
 	for (let i = 0; i < hrTags.length; i++){
        hrTags[i].classList.remove("dark-mode-hr", "colored-mode-hr");
-	}
-})
+	};
+});
 
 coloredButton.addEventListener("click", ()=>{
 	bodyTag.classList.remove("dark-mode");
@@ -21,7 +21,7 @@ coloredButton.addEventListener("click", ()=>{
 	for (let i = 0; i < hrTags.length; i++){
 		hrTags[1].classList.remove("dark-mode-hr");
 		hrTags[i].classList.add("colored-mode-hr");
-	}
+	};
 });
 
 darkModeButton.addEventListener("click", ()=>{
@@ -32,5 +32,5 @@ darkModeButton.addEventListener("click", ()=>{
 	for (let i = 0; i < hrTags.length; i++){
 		hrTags[i].classList.remove("colored-mode-hr");
         hrTags[i].classList.add("dark-mode-hr");
-	}
+	};
 });
